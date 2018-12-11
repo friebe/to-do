72fd9469f504008f7f4523d0e3ab10592e134d38
Jan Friebe <friebe@odas.biz>
2018-12-11 08:26:12 +0100

    init

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql');
+
+const SELECT_ALL_TODO_ITEMS = 'SELECT * FROM todo'
+const connection =  mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todo'
+});
+
+connection.connect(err => {
+    if(err) {
+        return err;
+    }
+});
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('fsdf')
+});
+
+app.get('/toDos', (req, res) => {
+    connection.query(SELECT_ALL_TODO_ITEMS, (err, results) => {
+        if(err) {
+            return res.send(err)
+        }
+        else {
+            return res.json({
+                data: results
+            })
+        }
+    })
+})
+
+app.listen(4000, () => {
+    console.log('online')
+});
